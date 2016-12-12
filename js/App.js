db550e814189e829148aa32e3a3de88ecf9918c1
glaphire <daria.motorina@gmail.com>
2016-12-13 00:12:19 +0200

    Added news component

--- js/App.js
+++ js/App.js
@@ -1,8 +1,17 @@
 var App = React.createClass({
 	render: function () {
 		return (
-			<div className="app"> Hello everyone, I*m App Component</div>
+			<div className="app"> Hello everyone, I*m App Component. I can render News.
+				<News />
+			</div>
 			);
+	}
+});
+
+var News = React.createClass({
+	render: function () {
+		return (<div className="news">It's pity, but there are no news.</div>
+		);
 	}
 });
 
