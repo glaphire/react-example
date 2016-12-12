252cf8fe8c8461b8a22e32bae277d0da60c63766
glaphire <daria.motorina@gmail.com>
2016-12-13 00:15:51 +0200

    Added comments component

--- js/App.js
+++ js/App.js
@@ -3,6 +3,7 @@ var App = React.createClass({
 		return (
 			<div className="app"> Hello everyone, I*m App Component. I can render News.
 				<News />
+				<Comments />
 			</div>
 			);
 	}
@@ -12,6 +13,12 @@ var News = React.createClass({
 	render: function () {
 		return (<div className="news">It's pity, but there are no news.</div>
 		);
+	}
+});
+
+var Comments = React.createClass({
+	render: function(){
+		return (<div className="comments">There is nothing to comment</div>);
 	}
 });
 
