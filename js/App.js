2a4b2f3dad5c4ef50b5ca4dcc8108c3a94121b34
glaphire <daria.motorina@gmail.com>
2016-12-13 01:40:32 +0200

    start learning component states

--- js/App.js
+++ js/App.js
@@ -1,17 +1,20 @@
 var my_news = [
-	{
-		author: 'Саша Печкин',
-		text: 'В четверг, четвертого числа...'
-	},
-	{
-		author: 'Просто Вася',
-		text: 'Считаю, что $ должен стоить 35 рублей!'
-	},
-	{
-		author: 'Гость',
-	text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
-	}
-];
+{
+	author: 'Саша Печкин',
+	text: 'В четчерг, четвертого числа...',
+	bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+},
+{
+	author: 'Просто Вася',
+	text: 'Считаю, что $ должен стоить 35 рублей!',
+	bigText: 'А евро 42!'
+},
+{
+	author: 'Гость',
+	text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+	bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+}
+];;
 
 
 var App = React.createClass({
@@ -29,14 +32,14 @@ var News = React.createClass({
 	propTypes: {
 		data: React.PropTypes.array.isRequired
 	},
+	
 	render: function () {
 		var data = this.props.data;
 		if (data.length > 0) {
 			var newsTemplate = data.map(function(item, index) {
 				return (
 					<div key={index}>
-						<p className="news__author">{item.author}:</p>
-						<p className="news__text">{item.text}</p>
+            			<Article data={item} />
 					</div>
 				);
 			});
@@ -51,6 +54,34 @@ var News = React.createClass({
 var Comments = React.createClass({
 	render: function(){
 		return (<div className="comments">There is nothing to comment</div>);
+	}
+});
+
+var Article = React.createClass({
+	propTypes: {
+		data: React.PropTypes.shape({
+		author: React.PropTypes.string.isRequired,
+		text: React.PropTypes.string.isRequired,
+		bigText: React.PropTypes.string.isRequired
+		})
+	},
+	getInitialState: function() {
+		return {
+			visible: false
+		};
+	},
+	render: function() {
+	var author = this.props.data.author,
+	text = this.props.data.text,
+	bigText = this.props.data.bigText;
+	return (
+		<div className='article'>
+			<p className='news__author'>{author}:</p>
+			<p className='news__text'>{text}</p>
+			<a href="#" className='news__readmore '>Подробнее</a>
+			<p className='news__big-text'>{bigText}</p>
+		</div>
+		)
 	}
 });
 
