5abc23842937a2cb398ff1fe533c7008530f4d5b
glaphire <daria.motorina@gmail.com>
2016-12-13 15:13:44 +0200

    added onClick handler

--- js/App.js
+++ js/App.js
@@ -70,16 +70,21 @@ var Article = React.createClass({
 			visible: false
 		};
 	},
+	readmoreClick: function(e){
+		e.preventDefault();
+		this.setState({visible:true});
+	},
 	render: function() {
 	var author = this.props.data.author,
 	text = this.props.data.text,
-	bigText = this.props.data.bigText;
+	bigText = this.props.data.bigText,
+	visible = this.state.visible;
 	return (
 		<div className='article'>
 			<p className='news__author'>{author}:</p>
 			<p className='news__text'>{text}</p>
-			<a href="#" className='news__readmore '>Подробнее</a>
-			<p className='news__big-text'>{bigText}</p>
+			<a href="#" onClick={this.readmoreClick} className={'news__readmore ' + (visible ? 'none' : '')}>Подробнее</a>
+			<p className={'news__big-text ' + (visible ? '' : 'none')}>{bigText}</p>
 		</div>
 		)
 	}
